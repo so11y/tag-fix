fe033ecd311ae30947bd2d207e10268bdaeacea5
sorry <247076126@qq.com>
2023-08-01 21:01:22 +0800

    🎈 perf: 跳过不必要的节点)

--- src/util/parse.ts
+++ src/util/parse.ts
@@ -1,7 +1,6 @@
-import { baseParse, ElementNode,RootNode } from "@vue/compiler-core";
+import { baseParse, ElementNode, RootNode } from "@vue/compiler-core";
 
 const igNoreTag = ["script", "style"];
-
 
 export function parseHtml(domString: string) {
   return baseParse(domString, {
@@ -14,17 +13,18 @@ export function findInRangeHtml(
   document: RootNode,
   start: number,
   end?: number
-):ElementNode | undefined {
+): ElementNode | undefined {
   end === undefined ? (end = start) : end;
-  const nodePaths:Array<ElementNode>  = [];
+  const nodePaths: Array<ElementNode> = [];
   const walk = (nodes: Array<ElementNode>) => {
     nodes.forEach((node) => {
       const startIndex = node.loc.start.offset;
       const endIndex = node.loc.end.offset;
-      if (startIndex! <= start  && endIndex >= start) {
-        nodePaths.push(node);
+      if (!(startIndex <= start && endIndex >= start)) {
+        return;
       }
-      if (node.children && startIndex! <= start) {
+      nodePaths.push(node);
+      if (node.children) {
         walk(node.children as Array<ElementNode>);
       }
     });
@@ -32,7 +32,7 @@ export function findInRangeHtml(
 
   walk(document.children as Array<ElementNode>);
   const lastNode = nodePaths.pop();
-  if(nodePaths.some((node)=>igNoreTag.includes(node.tag))){
+  if (nodePaths.some((node) => igNoreTag.includes(node.tag))) {
     return undefined;
   }
   return lastNode;
